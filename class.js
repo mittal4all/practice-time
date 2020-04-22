e5ef38c6224431baba0397b2b3bd2b34b24114f3
mittal4all <53186617+mittal4all@users.noreply.github.com>
2020-04-22 16:28:28 +0530

    Update class.js

--- class.js
+++ class.js
@@ -1,11 +1,4 @@
-class Do(){
-  Costructor(name,age){
-  this.name=name;
-  this.age=age;
-  }
-}
-const data=new Do("ankit",34);
-
+//inheritance 
 class Do1 extends Do(){
   Constructor(name,age,score){
   super(name,age)
